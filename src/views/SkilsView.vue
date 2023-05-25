2a8f0d34d4b1207affdbece08b9f4e62fe24c371
Darynka Tapia <darytapia09@gmail.com>
2023-05-25 00:15:49 -0600

    styles in my skils

--- src/views/SkilsView.vue
+++ src/views/SkilsView.vue
@@ -1,9 +1,7 @@
 <template>
   <h1 class="section-title">My Skils</h1>
-  <p class="in-construction">In construction 🚧</p>
-
   <div class="skils">
-    <span v-for="skil in skils" :key="skil">
+    <span class="chips" v-for="skil in skils" :key="skil">
       {{ skil.name }}
     </span>
   </div>
@@ -17,6 +15,7 @@
     {icon: '', name: 'css'},
     {icon: '', name: 'javascript'},
     {icon: '', name: 'typescript'},
+    {icon: '', name: 'node.js'},
     {icon: '', name: 'tailwindcss'},
     {icon: '', name: 'bootstrap'},
     {icon: '', name: 'less'},
@@ -24,12 +23,11 @@
     {icon: '', name: 'java'},
     {icon: '', name: 'php'},
     {icon: '', name: 'laravel'},
-    {icon: '', name: 'node.js'},
     {icon: '', name: 'jwt'},
-    {icon: '', name: 'dart'},
     {icon: '', name: 'netlify'},
     {icon: '', name: 'firebase'},
     {icon: '', name: 'flutter'},
+    {icon: '', name: 'dart'},
     {icon: '', name: 'unity'},
     {icon: '', name: 'blender'},
     {icon: '', name: 'figma'},
@@ -53,13 +51,24 @@
   @apply text-[#BFACE2] text-4xl mb-7 sm:text-6xl sm:mb-12 sm:mt-4;
 }
 .skils{
-  @apply text-white flex flex-wrap gap-4 h-[calc(100%-108px)] sm:h-[calc(100%-150px)] overflow-auto	;
-  & span{
-    @apply px-2 py-1 rounded-lg uppercase text-sm;
-  }
+  @apply flex flex-wrap justify-center gap-3 mt-5 overflow-auto h-[calc(100%-114px)] sm:h-auto sm:py-3;
 }
 
-.in-construction{
-  @apply w-full flex flex-col justify-center items-center text-white text-xl;
+.chips {
+  @apply flex items-center justify-center bg-[#301E67] text-white py-2 px-3 rounded-[20px]
+  text-sm uppercase opacity-0 cursor-default;
+  animation: fade-in 1s forwards;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+  box-shadow: 0 2px 5px rgba(255, 255, 255, 0.2);
+}
+    
+.chips:hover {
+  @apply bg-[#BFACE2] text-[#301E67];
+  transform: scale(1.05);
+}
+
+@keyframes fade-in {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
 }
 </style>
